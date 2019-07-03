b2e3e16f32d2a69e0231db3c6480541f47d76585
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-03 12:41:03 +0200

    Fix for create table: remove UNIQUE value from city and nickname

--- sqlQueries/db.sql
+++ sqlQueries/db.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS mentors (
     id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    nick_name TEXT NOT NULL UNIQUE,
+    nick_name TEXT NOT NULL,
     phone_number TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
-    city TEXT NOT NULL UNIQUE,
+    city TEXT NOT NULL,
     favourite_number BIGINT
 );
 
