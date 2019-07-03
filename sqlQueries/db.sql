8e5918e996c7970e9d31a04d0ad71487743d6d1e
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-03 10:38:01 +0200

    Add queries for create tables: mentors, applicants

---
+++ sqlQueries/db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS mentors (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    nick_name TEXT NOT NULL UNIQUE,
+    phone_number TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    city TEXT NOT NULL UNIQUE,
+    favourite_number BIGINT
+);
+
+CREATE TABLE IF NOT EXISTS applicants (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    phone_number TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    application_code BIGINT
+);
