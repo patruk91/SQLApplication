94916d869ded2500cbe53e1e058c3c9732d1e9f4
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-03 09:18:37 +0200

    Remove try/except block: to handle exception in each method

--- src/main/java/dao/SQL/DatabaseConnection.java
+++ src/main/java/dao/SQL/DatabaseConnection.java
@@ -5,15 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    public static Connection getConnection(String url, String user, String password) {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-            connection.setAutoCommit(true);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
+    public static Connection getConnection(String url, String user, String password) throws SQLException {
+        Connection connection = DriverManager.getConnection(url, user, password);
+        connection.setAutoCommit(true);
         return connection;
     }
 }
